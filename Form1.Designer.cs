7349e6fc76cce061584ded02e7cfe0955a8180e6
Thanh Huyen <letthanhhuyen@dtu.edu.vn>
2025-11-29 20:05:23 +0700

    Initial commit: upload existing StudentManager project

---
+++ Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace Quản_lý_thư_viện
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblBookld = new System.Windows.Forms.Label();
+            this.txtBookid = new System.Windows.Forms.TextBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.lblQuantity = new System.Windows.Forms.Label();
+            this.txtQuantity = new System.Windows.Forms.TextBox();
+            this.btnReturn = new System.Windows.Forms.Button();
+            this.btnBorrow = new System.Windows.Forms.Button();
+            this.btnAddBook = new System.Windows.Forms.Button();
+            this.listBooks = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lblBookld
+            // 
+            this.lblBookld.AutoSize = true;
+            this.lblBookld.Location = new System.Drawing.Point(12, 36);
+            this.lblBookld.Name = "lblBookld";
+            this.lblBookld.Size = new System.Drawing.Size(49, 13);
+            this.lblBookld.TabIndex = 0;
+            this.lblBookld.Text = "Book ID:";
+            // 
+            // txtBookid
+            // 
+            this.txtBookid.Location = new System.Drawing.Point(67, 33);
+            this.txtBookid.Multiline = true;
+            this.txtBookid.Name = "txtBookid";
+            this.txtBookid.Size = new System.Drawing.Size(155, 20);
+            this.txtBookid.TabIndex = 1;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Location = new System.Drawing.Point(12, 66);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(30, 13);
+            this.lblTitle.TabIndex = 2;
+            this.lblTitle.Text = "Title:";
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(67, 63);
+            this.txtTitle.Multiline = true;
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(155, 20);
+            this.txtTitle.TabIndex = 3;
+            // 
+            // lblQuantity
+            // 
+            this.lblQuantity.AutoSize = true;
+            this.lblQuantity.Location = new System.Drawing.Point(12, 106);
+            this.lblQuantity.Name = "lblQuantity";
+            this.lblQuantity.Size = new System.Drawing.Size(49, 13);
+            this.lblQuantity.TabIndex = 4;
+            this.lblQuantity.Text = "Quantity:";
+            this.lblQuantity.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // txtQuantity
+            // 
+            this.txtQuantity.Location = new System.Drawing.Point(67, 103);
+            this.txtQuantity.Multiline = true;
+            this.txtQuantity.Name = "txtQuantity";
+            this.txtQuantity.Size = new System.Drawing.Size(155, 20);
+            this.txtQuantity.TabIndex = 5;
+            // 
+            // btnReturn
+            // 
+            this.btnReturn.Location = new System.Drawing.Point(174, 129);
+            this.btnReturn.Name = "btnReturn";
+            this.btnReturn.Size = new System.Drawing.Size(75, 23);
+            this.btnReturn.TabIndex = 6;
+            this.btnReturn.Text = "Return";
+            this.btnReturn.UseVisualStyleBackColor = true;
+            this.btnReturn.Click += new System.EventHandler(this.btnReturn_Click);
+            // 
+            // btnBorrow
+            // 
+            this.btnBorrow.Location = new System.Drawing.Point(93, 129);
+            this.btnBorrow.Name = "btnBorrow";
+            this.btnBorrow.Size = new System.Drawing.Size(75, 23);
+            this.btnBorrow.TabIndex = 7;
+            this.btnBorrow.Text = "Borrow";
+            this.btnBorrow.UseVisualStyleBackColor = true;
+            this.btnBorrow.Click += new System.EventHandler(this.btnBorrow_Click);
+            // 
+            // btnAddBook
+            // 
+            this.btnAddBook.Location = new System.Drawing.Point(12, 129);
+            this.btnAddBook.Name = "btnAddBook";
+            this.btnAddBook.Size = new System.Drawing.Size(75, 23);
+            this.btnAddBook.TabIndex = 8;
+            this.btnAddBook.Text = "Add Book";
+            this.btnAddBook.UseVisualStyleBackColor = true;
+            this.btnAddBook.Click += new System.EventHandler(this.btnAddBook_Click);
+            // 
+            // listBooks
+            // 
+            this.listBooks.FormattingEnabled = true;
+            this.listBooks.Location = new System.Drawing.Point(12, 158);
+            this.listBooks.Name = "listBooks";
+            this.listBooks.Size = new System.Drawing.Size(234, 95);
+            this.listBooks.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBooks);
+            this.Controls.Add(this.btnAddBook);
+            this.Controls.Add(this.btnBorrow);
+            this.Controls.Add(this.btnReturn);
+            this.Controls.Add(this.txtQuantity);
+            this.Controls.Add(this.lblQuantity);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.txtBookid);
+            this.Controls.Add(this.lblBookld);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblBookld;
+        private System.Windows.Forms.TextBox txtBookid;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.Label lblQuantity;
+        private System.Windows.Forms.TextBox txtQuantity;
+        private System.Windows.Forms.Button btnReturn;
+        private System.Windows.Forms.Button btnBorrow;
+        private System.Windows.Forms.Button btnAddBook;
+        private System.Windows.Forms.ListBox listBooks;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Quản_lý_thư_viện
+{
+    public partial class Form1 : Form
+    {
+        List<Book> books = new List<Book>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender , EventArgs e)
+        {
+            books.Add(new Book { BookId = " B001", Title = " C# cơ bản ", Quantity = 5 });
+            books.Add(new Book { BookId = " B002", Title = " python nâng cao", Quantity = 3 });
+            books.Add(new Book { BookId = " B003", Title = "Java cho người mới", Quantity = 2 });
+            ShowBooks();
+        }
+    
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAddBook_Click(object sender, EventArgs e)
+        {
+            Book b = new Book();
+            b.BookId = txtBookid.Text;
+            b.Title = txtTitle.Text;
+            b.Quantity = int.Parse(txtQuantity.Text);
+            books.Add(b);
+            ShowBooks();
+            MessageBox.Show(" Thêm sách thành công!");
+        }
+
+        private void btnBorrow_Click(object sender, EventArgs e)
+        {
+            if (listBooks.SelectedIndex < 0)
+            {
+                MessageBox.Show(" hãy chọn một cuốn sách!");
+                return;
+            }
+            Book b = books[listBooks.SelectedIndex];
+            if (b.Quantity > 0)
+            {
+                b.Quantity--;
+                ShowBooks();
+                MessageBox.Show("Mượn sách thanh công!");
+            }
+            else
+            {
+                MessageBox.Show(" Sách đã hết!");
+            }
+        }
+        private void btnReturn_Click(object sender, EventArgs e)
+        {
+            if (listBooks.SelectedIndex < 0)
+            {
+                MessageBox.Show("Hãy chọn một cuốn sách để trả!");
+                return;
+            }
+            Book b = books[listBooks.SelectedIndex];
+            b.Quantity++;
+            ShowBooks();
+            MessageBox.Show("Trả sách thanh công!");
+        }
+        private void ShowBooks()
+        {
+            listBooks.Items.Clear();
+            foreach (var book in books)
+            {
+                listBooks.Items.Add($"{book.BookId} - {book.Title} (Qty:{book.Quantity}");
+            }
+        }
+        public class Book
+        {
+            public string BookId { get;set;}
+            public string Title {  get;set;}
+            public int Quantity { get;set;}
+        }
+    }
+}
